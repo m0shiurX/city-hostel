72bd72c75d1f3bfa83a15c6d641f1e0e44d6d594
Moshiur Rahman <unimrgm@gmail.com>
2023-11-07 23:03:51 +0600

    tenancy updated

--- app/Traits/MultiTenantModelTrait.php
+++ app/Traits/MultiTenantModelTrait.php
@@ -11,6 +11,7 @@ trait MultiTenantModelTrait
     {
         if (!app()->runningInConsole() && auth()->check()) {
             $isAdmin = auth()->user()->roles->contains(1);
+            $isHost = auth()->user()->roles->contains(2);
             static::creating(function ($model) use ($isAdmin) {
                 // Prevent admin from setting his own id - admin entries are global.
                 // If required, remove the surrounding IF condition and admins will act as users
@@ -18,13 +19,13 @@ trait MultiTenantModelTrait
                     $model->created_by_id = auth()->id();
                 }
             });
-            // if (!$isAdmin) {
-            //     static::addGlobalScope('created_by_id', function (Builder $builder) {
-            //         $field = sprintf('%s.%s', $builder->getQuery()->from, 'created_by_id');
+            if (!$isAdmin && $isHost) {
+                static::addGlobalScope('created_by_id', function (Builder $builder) {
+                    $field = sprintf('%s.%s', $builder->getQuery()->from, 'created_by_id');
 
-            //         $builder->where($field, auth()->id())->orWhereNull($field);
-            //     });
-            // }
+                    $builder->where($field, auth()->id())->orWhereNull($field);
+                });
+            }
         }
     }
 }
