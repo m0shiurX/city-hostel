c8fd380ea908815ed30e12c56361ec368c0d6e87
Moshiur Rahman <unimrgm@gmail.com>
2023-11-08 15:21:44 +0600

    update student permission

--- database/seeders/PermissionRoleTableSeeder.php
+++ database/seeders/PermissionRoleTableSeeder.php
@@ -14,7 +14,7 @@ class PermissionRoleTableSeeder extends Seeder
         Role::findOrFail(1)->permissions()->sync($admin_permissions->pluck('id'));
 
         $host_permissions = [17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 32, 33, 35, 42, 43, 45, 58];
-        $student_permissions = [31, 33, 35, 42, 43, 45, 58];
+        $student_permissions = [31, 33, 35, 41, 42, 43, 45, 58];
 
         Role::findOrFail(2)->permissions()->sync($host_permissions);
         Role::findOrFail(3)->permissions()->sync($student_permissions);
